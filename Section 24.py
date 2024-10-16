9ec72a928ac6f845225f88a0f7be3b1282eadbb8
Alifseen <alifseen@gmail.com>
2024-10-16 17:46:29 +0500

    initial commit + Pseudo code

---
+++ Section 24.py
@@ -0,0 +1,23 @@
+"""PDF Template Maker
+We get introduced to this concept by creating a single page before moving on to using data processing for template creation
+"""
+
+"""
+for loop over csv row
+    add page
+    set font
+    set color
+    create cell
+    add line
+    add space using ln
+    set csv row topic as header
+    for loop over pg value using i=1 i<pg i++ or range for loops
+        add page
+        add space using ln
+        add line
+        set font
+        set color
+        create cell footer
+        create cell page number
+        set csv row topic as footer
+"""
